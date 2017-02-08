3361f669d326d376c3d512ec38cfa1ac7438dea1
Duncan Lewis <duncan@piranhr.com>
2017-02-08 02:25:46 +0000

    Extending country functionality

--- controller/CountryController.php
+++ controller/CountryController.php
@@ -31,9 +31,16 @@ class CountryController extends Controller
      */
     public function view($id) {
 
-        $country = $this->Country->find($id);
+        $country = $this->Country->findById($id);
 
         $this->set('country', $country);
+
+        //Register City class so we can lookup the countries biggest cities
+        //ToDo: implement the Limit in the getLargest method
+        $City = new City();
+        $largestCities = $City->getLargest($country['A3Code'], 5);
+
+        $this->set('largestCities', $largestCities);
     }
 
 
@@ -46,14 +53,26 @@ class CountryController extends Controller
      */
     public function search($query) {
 
+        $query = $_POST['countryQuery'];
+
         //Check if string is under 3 characters long, if so then error ToDo: check if this needs to be less than or <=
         if (strlen($query) < 3) {
             echo "Please enter search query longer than 3 characters."; //Todo make a proper error handler
         }
 
+        //ToDo: Like isnt the best operator here, perhaps try implementing wildcard
+        $conditions = array(
+            'Name LIKE' => $query
+        );
+
         //Call to the model method for search
+        $countryList = $this->Country->find('all', $conditions);
 
         //Return results to the page
+        $this->set('countryList', $countryList);
+
+        //Also return the original query to show some context for user
+        $this->set('searchQuery', $query);
 
     }
 

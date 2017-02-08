0b0a66d764ca658290b735ccad5a6ad32261a6b6
Duncan Lewis <duncan@piranhr.com>
2017-02-08 02:27:04 +0000

    Added city functionality

---
+++ model/City.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: duncan
+ * Date: 08/02/2017
+ * Time: 01:58
+ */
+class City extends Model
+{
+
+    protected $_primary = "ID";
+
+    /**
+     * getLargest
+     *
+     * Gets the largest cities (by population)
+     *
+     * @param null $country string City - Specify a country to restrict results to (A3Code Only)
+     * @param $limit int City - Limit number of results to be returned
+     * @return array City list
+     */
+    public function getLargest($country = null, $limit) {
+
+        if (isset($country)) {
+            $conditions = array(
+                'A3Code LIKE' => $country
+            );
+        }
+
+        $result = $this->find('all', $conditions);
+
+        return $result;
+
+    }
+
+}
